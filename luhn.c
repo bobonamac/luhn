acd8bab5acb7eea0c30be6845b75f84a89cfbcb5
bobonamac <bobviens@gmail.com>
2020-01-29 20:14:44 +0100

    addrd functions

--- luhn.c
+++ luhn.c
@@ -7,16 +7,35 @@ validity using the Luhn algorithm
 
 #define MAX_DIGITS 16
 
+int getCardNum(char * creditCardNumber);
+int doubleSecondDigits(char * creditCardNumber, char * doubleEveryOther);
+int verifyCardNum(char * creditCardNumber, char * doubleEveryOther, int sumDigits);
+
 int main(void){
 
 	char creditCardNumber[MAX_DIGITS];
 	char doubleEveryOther[MAX_DIGITS];
 	int sumDigits;
 
+	getCardNum(creditCardNumber);
+
+	doubleSecondDigits(creditCardNumber, doubleEveryOther);
+
+	verifyCardNum(creditCardNumber, doubleEveryOther, sumDigits);
+
+return 0;
+}
+
+// ____________________________________________________________________________
+
+int getCardNum(char * creditCardNumber){
 	// prompts user for card number
 	printf("16-digit credit card number: ");
 	scanf("%16s", creditCardNumber);
+	return 0;
+}
 
+int doubleSecondDigits(char * creditCardNumber, char * doubleEveryOther){
 	// converts ascii and doubles every second digit
 	for (int i = 0; i < MAX_DIGITS; i++){
 		doubleEveryOther[i] = (creditCardNumber[i] - 48);
@@ -27,13 +46,14 @@ int main(void){
 			}
 		}
 	}
+return 0;
+}
 
+int verifyCardNum(char * creditCardNumber, char * doubleEveryOther, int sumDigits){
 	// adds every second number for check digit
-	sumDigits = (doubleEveryOther[1] +
-		doubleEveryOther[3] + doubleEveryOther[5] +
-		doubleEveryOther[7] + doubleEveryOther[9] +
-		doubleEveryOther[11] + doubleEveryOther[13] +
-		doubleEveryOther[15]);
+	sumDigits = (doubleEveryOther[1] + doubleEveryOther[3] + doubleEveryOther[5]
+		 + doubleEveryOther[7] + doubleEveryOther[9] + doubleEveryOther[11] + 
+		 doubleEveryOther[13] + doubleEveryOther[15]);
 
 	// prints check digit and card number for verdict
 	printf("Check Digit: %d\n", sumDigits);
@@ -46,6 +66,5 @@ int main(void){
 		printf(" is a valid number.\n");
 	}
 	else printf(" is not a valid number.\n");
-
-return 0;
+return 0;	
 }
